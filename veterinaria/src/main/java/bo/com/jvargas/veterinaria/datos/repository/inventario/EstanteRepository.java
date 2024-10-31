5e357076ffeb57891525674777b8b041707917dd
Gerson <villarroelpoma2@gmail.com>
2024-10-30 23:45:56 -0400

    repositorio para estante

---
+++ veterinaria/src/main/java/bo/com/jvargas/veterinaria/datos/repository/inventario/EstanteRepository.java
@@ -0,0 +1,11 @@
+package bo.com.jvargas.veterinaria.datos.repository.inventario;
+
+import bo.com.jvargas.veterinaria.datos.model.Estante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author GERSON
+ */
+public interface EstanteRepository extends JpaRepository<Estante, Long> {
+
+}
